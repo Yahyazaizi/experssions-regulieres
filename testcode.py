a8d5e8ac14f0ad5a3c44b7c09848ac215e5b1586
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2023-12-14 20:45:05 +0100

    Add files via upload

---
+++ testcode.py
@@ -0,0 +1,20 @@
+from experssions_regulieres  import recherche
+print(recherche(input("svp done un mot: "),(input("svp done un chaine de mot: "))))
+
+
+
+from experssions_regulieres  import verifie_chaine
+print(verifie_chaine(input("svp done un mot: ")))
+  
+
+from experssions_regulieres  import remplace
+print(remplace(input("svp done un mot: ")))
+
+
+from experssions_regulieres  import verifie_numero
+print(verifie_numero(input("svp done un numero: ")))
+
+
+
+from experssions_regulieres  import verifie_email
+print(verifie_email(input("svp done un email: ")))
